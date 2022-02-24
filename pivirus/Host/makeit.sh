15ee25fd99ae07099410e7ef49787ad03b586911
iTeraTorr <ruchir.mehta2112@gmail.com>
2022-02-24 16:40:21 +0530

    pivirus

---
+++ pivirus/Host/makeit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+gcc -Wall -g -no-pie ./sweet.c -o ./sweet
+#objdump -d -M intel ./sweet > disassembly_sweet.txt
+#./sweet
+#readelf -a sweet > readelf.txt
+#objdump -d sweet > disassembly_AT_and_T.txt
+#objdump -M intel -d sweet > disassembly_intel.txt
+#strace -v -o stracedFile.txt ./sweet > output.txt
+#hexdump -v -C sweet > hexdump.txt
+#objdump --full-contents --section=.comment sweet
+# To read the content of a section in human readable form `readelf -p .dynstr ./sweet`
