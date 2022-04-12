6598d6ecd27eac2cf0e90bb5e8b3135744f9f684
iTeraTorr <ruchir.mehta2112@gmail.com>
2022-04-12 19:14:06 +0530

    Job

---
+++ Job_analysing_system_calls/empty/make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name=$(basename $1 .c)
+mkdir -p $name
+gcc -w "$1" -o $name/$name.elf
+strace -o $name/$name.strace ./$name/$name.elf
+awk -F'(' '{print $1}' $name/$name.strace > $name/$name.sc
+sort $name/$name.sc | uniq -c> $name/$name.uniq_sc
